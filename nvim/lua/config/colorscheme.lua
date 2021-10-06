a7c2a8f88e43fecc8fe819532ffbd4fe4d07beb7
Muhammad Ahmad 🌌 <mhdahmadx@gmail.com>
2021-10-06 15:48:29 +0300

    refactor(colorscheme): enable parnth highlight

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -16,11 +16,7 @@ nightfox.setup({
         variables = 'NONE', -- Style that is applied to variables: see `highlight-args` for options
     },
     inverse = {
-<<<<<<< HEAD
-        match_paren = false, -- Enable/Disable inverse highlighting for match parens
-=======
         match_paren = true, -- Enable/Disable inverse highlighting for match parens
->>>>>>> c115808 (updates)
         visual = false, -- Enable/Disable inverse highlighting for visual selection
         search = false, -- Enable/Disable inverse highlights for search highlights
     },
